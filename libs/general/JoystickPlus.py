a24cae12c0c131f3db7db113786c341a9f67701f
Asssikz <6asik9@gmail.com>
2025-07-15 19:16:26 +0500

    Add wrapper for all modules

---
+++ libs/general/JoystickPlus.py
@@ -0,0 +1,11 @@
+from machine import ADC, Pin
+
+class JoystickPlus(ADC):
+    def __init__(self, pinX, pinY):
+        self.X = ADC(Pin(pinX))
+        self.Y = ADC(Pin(pinY))
+
+    def read(self):
+        x = self.X.read_u16()
+        y = self.Y.read_u16()
+        return (x, y)
--- pibody.py
+++ pibody.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C, SoftI2C
+from machine import Pin, PWM, ADC, I2C, SoftI2C
 
 from BME280 import BME280  
 from MPU6050 import MPU6050
@@ -10,6 +10,7 @@ from libs.general.RotaryEncoder import RotaryEncoder
 from libs.general.NeoPixelPlus import NeoPixelPlus
 from libs.general.BuzzerPlus import BuzzerPlus
 from libs.general.ServoPlus import ServoPlus
+from libs.general.JoystickPlus import JoystickPlus
 from libs.DisplayPlus import DisplayPlus
 from libs.iot.WiFi import WiFi
 from libs.iot.telegram_bot import TelegramBot
@@ -25,9 +26,19 @@ _SLOT_MAP = {
     'H': (1, 18, 19),
 }
 
-def get_slot_pins(slot):
+_ADC_SLOT_MAP = {
+    'C': (None, 28, 22),
+    'F': (1, 26, 27)
+}
+
+# Functions-helpers
+def get_slot_pins(slot, adc=False, joystick=False):
     slot = slot.upper()[0]
-    if slot not in _SLOT_MAP:
+    if joystick and (slot != 'F'):
+        print(f"Joystick is not supported for slot '{slot}'. Recomendation: Use slot 'F' instead.")
+    elif adc and (slot not in _ADC_SLOT_MAP):
+        raise ValueError(f"Invalid slot '{slot}'. Use C or F (Other slots are not ADC-compatible)")
+    elif slot not in _SLOT_MAP:
         raise ValueError(f"Invalid slot '{slot}'. Use A, B, C, D, E, or F (C is not I2C-compatible)")
     bus, sda, scl = _SLOT_MAP[slot]
     return (bus, sda, scl)
@@ -39,6 +50,7 @@ def get_i2c(slot, soft_i2c=False):
     else:
         return I2C(bus, scl=Pin(scl), sda=Pin(sda))
 
+# i2c modules
 class ClimateSensor(BME280):
     def __init__(self, slot, soft_i2c=False):
         super().__init__(get_i2c(slot, soft_i2c))
@@ -60,10 +72,21 @@ class DistanceSensor(VL53L0X):
     def __init__(self, slot, soft_i2c=False):
         super().__init__(get_i2c(slot, soft_i2c))
 
+
+# displays 
 class OLED(SSD1306):
     def __init__(self, slot, soft_i2c=False, width=128, height=64):
         super().__init__(get_i2c(slot, soft_i2c), width=width, height=height)
 
+class Display(DisplayPlus):
+    def __init__(self):
+        super().__init__()
+
+class LEDTower(NeoPixelPlus):
+    def __init__(self, pin=8, led_num=8):
+        super().__init__(pin=pin, led_num=led_num)
+
+# general modules
 class EncoderSensor(RotaryEncoder):
     def __init__(self, slot):
         _, sda, scl = get_slot_pins(slot)
@@ -74,14 +97,48 @@ class Buzzer(BuzzerPlus):
         _, sda, _ = get_slot_pins(slot)
         super().__init__(pin=sda)
 
-class LEDTower(NeoPixelPlus):
-    def __init__(self, pin=8, led_num=8):
-        super().__init__(pin=pin, led_num=led_num)
-
-class Display(DisplayPlus):
-    def __init__(self):
-        super().__init__()
-
 class Servo(ServoPlus):
     def __init__(self, pin, freq=50):
         super().__init__(pin=pin, freq=freq)
+
+def LED(slot, pwm=False):
+    _, sda, _ = get_slot_pins(slot)
+    if not pwm:
+        return Pin(sda, Pin.OUT)
+    else:
+        return PWM(Pin(sda), freq=50)
+
+class PushButton(Pin):
+    def __init__(self, slot):
+        _, sda, _ = get_slot_pins(slot)
+        super().__init__(sda, Pin.IN)
+
+class Switch(Pin):
+    def __init__(self, slot):
+        _, sda, _ = get_slot_pins(slot)
+        super().__init__(sda, Pin.IN)
+
+class Touch(Pin):
+    def __init__(self, slot):
+        _, sda, _ = get_slot_pins(slot)
+        super().__init__(sda, Pin.IN)
+
+class MotionDetector(Pin):
+    def __init__(self, slot):
+        _, sda, _ = get_slot_pins(slot)
+        super().__init__(sda, Pin.IN)
+
+class LightSensor(ADC):
+    def __init__(self, slot):
+        _, sda, _ = get_slot_pins(slot, adc=True)
+        super().__init__(Pin(sda))
+
+class Potentiometer(ADC):
+    def __init__(self, slot):
+        _, sda, _ = get_slot_pins(slot, adc=True)
+        super().__init__(Pin(sda))
+
+class Joystick(JoystickPlus):
+    def __init__(self, slot):
+        _, sda, scl = get_slot_pins(slot, joystick=True)
+        super().__init__(pinX=sda, pinY=scl)
