3e2ed77d8ee25d75683f35786d033605bd9806e2
Max Erickson <minilie88@gmail.com>
2025-11-06 19:57:23 +0500

    Lazy import of all modules

--- pibody/__init__.py
+++ pibody/__init__.py
@@ -1,39 +1,104 @@
-from .wrappers.generic import LED, ButtonLike, ADC
-from .wrappers.i2c import ClimateSensor, ColorSensor, DistanceSensor, GyroAccel, OLED
-from .wrappers.modules import Buzzer, PWM, Joystick, Encoder, SoundSensor, LEDTower
-from .modules.Servo import Servo
+def __getattr__(name):
+    if name == "LED":
+        from .wrappers.generic import LED
+        return LED
+    if name in ("Button", "Switch", "TouchSensor", "MotionSensor"):
+        from .wrappers.generic import ButtonLike
+        return ButtonLike
+    if name in ("ADC", "LightSensor", "Potentiometer", "Pot", "Light"):
+        from .wrappers.generic import ADC
+        return ADC
+    if name in ("Climate", "ClimateSensor"):
+        from .wrappers.i2c import ClimateSensor
+        return ClimateSensor
+    if name in ("Color", "ColorSensor"):
+        from .wrappers.i2c import ColorSensor
+        return ColorSensor
+    if name in ("Distance", "DistanceSensor"):
+        from .wrappers.i2c import DistanceSensor
+        return DistanceSensor
+    if name in ("GyroAccel", "GyroAxel"):
+        from .wrappers.i2c import GyroAccel
+        return GyroAccel
+    if name == "OLED":
+        from .wrappers.i2c import OLED
+        return OLED
+    if name == "Buzzer":
+        from .wrappers.modules import Buzzer
+        return Buzzer
+    if name == "PWM":
+        from .wrappers.modules import PWM
+        return PWM
+    if name == "Joystick":
+        from .wrappers.modules import Joystick
+        return Joystick
+    if name == "Encoder":
+        from .wrappers.modules import Encoder
+        return Encoder
+    if name in ("SoundSensor", "Sound"):
+        from .wrappers.modules import SoundSensor
+        return SoundSensor
+    if name == "LEDTower":
+        from .wrappers.modules import LEDTower
+        return LEDTower
+    if name == "Servo":
+        from .modules.Servo import Servo
+        return Servo
+    if name == "WiFi":
+        from .iot.WiFi import WiFi
+        return WiFi
+    if name == "TelegramBot":
+        from .iot.telegram_bot import TelegramBot
+        return TelegramBot
+    if name == "display":
+        from .Display import display
+        return display
+    if name == "demo":
+        from .Demo import demo
+        return demo
+    
+    raise AttributeError(f"module {__name__} has no attribute {name}")
 
-from .iot.WiFi import WiFi
-from .iot.telegram_bot import TelegramBot
 
-from .Display import display
-from .Demo import demo
+# Fake import detection
+# Ignore this part of code, it's just to help IDEs with autocompletion
+try: 
+    _ = 1/0
+except:
+    FAKE_IMPORT = False
 
+if FAKE_IMPORT:
+    from .wrappers.generic import LED, ButtonLike, ADC
+    from .wrappers.i2c import ClimateSensor, ColorSensor, DistanceSensor, GyroAccel, OLED
+    from .wrappers.modules import Buzzer, PWM, Joystick, Encoder, SoundSensor, LEDTower
+    from .modules.Servo import Servo
 
-# Demo
+    from .iot.WiFi import WiFi
+    from .iot.telegram_bot import TelegramBot
 
-# Aliases 
+    from .Display import display
+    from .Demo import demo
 
-# Button Likes
-Button = ButtonLike
-Switch = ButtonLike
-TouchSensor = ButtonLike
-MotionSensor = ButtonLike
+    # Button Likes
+    Button = ButtonLike
+    Switch = ButtonLike
+    TouchSensor = ButtonLike
+    MotionSensor = ButtonLike
 
-# Analog Likes
-LightSensor = ADC
-Potentiometer = ADC
+    # Analog Likes
+    LightSensor = ADC
+    Potentiometer = ADC
 
-# Sensors
-Climate = ClimateSensor
-Color = ColorSensor
-Distance = DistanceSensor
-Touch = TouchSensor
-Motion = MotionSensor
-Light = LightSensor
-Pot = Potentiometer
-Sound = SoundSensor
+    # Sensors
+    Climate = ClimateSensor
+    Color = ColorSensor
+    Distance = DistanceSensor
+    Touch = TouchSensor
+    Motion = MotionSensor
+    Light = LightSensor
+    Pot = Potentiometer
+    Sound = SoundSensor
 
-#Other
-GyroAxel = GyroAccel
+    #Other
+    GyroAxel = GyroAccel
 
