ef982a65ccccef0f8fe1e8df6bfef4c4c7ce01f2
Asssikz <6asik9@gmail.com>
2026-01-22 19:09:35 +0500

    pwm bug fix

--- pibody/modules/PWM.py
+++ pibody/modules/PWM.py
@@ -3,14 +3,14 @@ from machine import Pin
 
 
 def u16_to_float(u16 : int):
-    return u16 / 65536
+    return u16 / 65535
 
 class PWM(_BASE_PWM):    
    
     def __init__(self, pin:int):        
         super().__init__(Pin(pin))
         self._duty = u16_to_float(self.duty_u16())
-
+        self._u16_duty = self.duty_u16()
 
     def duty(self, duty_cycle:float|None = None):
         """Set duty_cycle of PWM object in range from 0 to 1. """
@@ -18,6 +18,7 @@ class PWM(_BASE_PWM):
             return self._duty
         if duty_cycle < 0 or duty_cycle > 1: 
             raise ValueError("duty_cycle must be in range from 0 to 1")
-        self._duty = int(duty_cycle * 65536)
-        return self.duty_u16(self._duty)
+        self._duty = duty_cycle
+        self._u16_duty = int(self._duty * 65535)
+        self.duty_u16(self._u16_duty)
 
